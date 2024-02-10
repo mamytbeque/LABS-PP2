2ed583d893db21386027815a606c3d981e9b528f
mamytbeque <156942793+mamytbeque@users.noreply.github.com>
2024-02-10 17:53:03 +0600

    Create ex2.py

---
+++ Lab3/Functions/ex2.py
@@ -0,0 +1,13 @@
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
+
+
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+
+
+celsius = fahrenheit_to_celsius(fahrenheit)
+
+
+print("Equivalent temperature in Celsius:", celsius)
+ 
