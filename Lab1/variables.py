88a8afe11194f8a9a91afd61ab2bad7ccf5f87c4
mamytbeque <156942793+mamytbeque@users.noreply.github.com>
2024-02-10 17:22:49 +0600

    Create variables.py

---
+++ Lab1/variables.py
@@ -0,0 +1,123 @@
+x = 5
+print(type(x))
+x = "Hello World"
+x = 20
+x = 20.5
+x = 1j
+x = ["apple", "banana", "cherry"]
+x = ("apple", "banana", "cherry")
+x = range(6)
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = b"Hello"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = None
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("apple", "banana", "cherry"))
+x = tuple(("apple", "banana", "cherry"))
+x = range(6)
+x = dict(name="John", age=36)
+x = set(("apple", "banana", "cherry"))
+x = frozenset(("apple", "banana", "cherry"))
+x = bool(5)
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
+
+x = 5
+print(type(x))
+int
+
+x = "Hello World"
+print(type(x))
+str
+
+x = 20.5
+print(type(x))
+float
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+
+x = True
+print(type(x))
+bool
