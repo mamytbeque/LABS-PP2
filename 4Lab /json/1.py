212605df228c846982c0c14ac78d8f0ac002d3a0
mamytbeque <156942793+mamytbeque@users.noreply.github.com>
2024-02-21 00:38:32 +0600

    Delete 4Lab /json directory

--- 4Lab /json/1.py
+++
@@ -1,6 +0,0 @@
-import json
-file = open('H:\pp2\Labs\Lab4\JSON\data-sample.json')
-data_base = json.load(file)
-print("Interface Status")
-print("================================================================================")
-print("DN                                  x.                Description           Speed    MTU  ")
