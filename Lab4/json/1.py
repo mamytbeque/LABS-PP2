c3d63451f5b586a2be5a684acdf4ae88553a2067
mamytbeque <156942793+mamytbeque@users.noreply.github.com>
2024-02-21 00:40:35 +0600

    Create 1.py

---
+++ Lab4/json/1.py
@@ -0,0 +1,6 @@
+import json
+file = open('C:\pp2\Labs\Lab4\JSON\data-sample.json')
+data_base = json.load(file)
+print("Interface Status")
+print("================================================================================")
+print("DN                                  x.                Description           Speed    MTU  ")
